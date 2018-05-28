92d40cba2fbdaf63be08c7261a1a290720858ba9
losthdpenguin <thomas@flanaghan.net>
2018-05-28 22:59:58 +0100

    added Correct / Incorrect
    If you get the password and username correct / incorrect it will state it to you

--- Login.py
+++ Login.py
@@ -8,9 +8,14 @@ username = ''
 
 # While statement
 while username  !=(usercreate):
-    print('What is the password?')
+    print('What is the Username?')
 #have to have this otherwise continuously says what is the password
     username = input()
+#if statement if the password is incorrect then it will retry otherwise it will say correct then ask for password
+    if username !=(usercreate):
+        print("Incorrect")
+    else:
+        print("Correct")
 
 #password
 
@@ -24,3 +29,8 @@ while password  !=(passcreate):
     print('What is the password?')
 #have to have this otherwise continuously says what is the password
     password = input()
+    if password !=(passcreate):
+        print("Incorrect")
+    else:
+        print("Correct")
+
